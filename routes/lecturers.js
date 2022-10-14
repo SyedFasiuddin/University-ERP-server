8050eef1207d589e2e86d4e414dc6cf92680a7d4
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2022-10-14 19:58:30 +0530

    feat: better code organization
    Things done here:
      - added controllers for already present routes
      - separated the controller and routes

--- routes/lecturers.js
+++ routes/lecturers.js
@@ -1,105 +1,11 @@
 const express = require("express")
+const { getAllLecturers, getLecturerById, deleteLecturerById, addLecturer } = require("../controller/lecturers")
 const router = express.Router()
-const db = require("../db")
 
-router.get("/", async (_, res) => {
-    try {
-        const queryRes = await db.query("SELECT * FROM lecturers")
-        res.send({ ...queryRes.rows })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
-})
-
-router.get("/:id", async (req, res) => {
-    try {
-        const queryRes = await db.query("SELECT * FROM lecturers WHERE lecturer_id = $1", [req.params.id])
-        if (queryRes.rows.length > 0 )
-            res.send({ ...queryRes.rows })
-        else
-            res.send({
-                "message": "Cannot find lecturer with id " + req.params.id
-            })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
-})
-
-router.post("/:id", async (req, res) => {
-    const query = {
-        text: `INSERT INTO lecturers (
-                    lecturer_id,
-                    name,
-                    fathers_name,
-                    spouse_name,
-                    phone_num,
-                    dob,
-                    email,
-                    address,
-                    qualification,
-                    subject_expertise,
-                    department,
-                    experience,
-                    aadhar_number,
-                    pan_number,
-                    bank_account_number,
-                    pay_scale,
-                    basic_pay,
-                    gross_salary,
-                    deduction,
-                    net_salary )
-                VALUES (
-                  $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20
-                )
-                 RETURNING *`,
-        values: [
-            req.body.lecturer_id,
-            req.body.name,
-            req.body.fathers_name,
-            req.body.spouse_name,
-            req.body.phone_num,
-            req.body.dob,
-            req.body.email,
-            req.body.address,
-            req.body.qualification,
-            req.body.subject_expertise,
-            req.body.department,
-            req.body.experience,
-            req.body.aadhar_number,
-            req.body.pan_number,
-            req.body.bank_account_number,
-            req.body.pay_scale,
-            req.body.basic_pay,
-            req.body.gross_salary,
-            req.body.deduction,
-            req.body.net_salary
-        ]
-    }
-
-    try {
-        const queryRes = await db.query(query)
-        res.send({ ...queryRes.rows })
-    } catch (e) {
-        console.error(e.stack)
-        res.status(400)
-        res.send(e.details)
-    }
-})
-
-router.delete("/:id", async (req, res) => {
-    try {
-        const queryRes = await db.query("DELETE FROM lecturers WHERE lecturer_id = $1", [req.params.id])
-        res.send({ ...queryRes.rows })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
-})
+router.get("/", getAllLecturers)
+router.get("/:id", getLecturerById)
+router.post("/:id", addLecturer)
+router.delete("/:id", deleteLecturerById)
 
 module.exports = router
 
--- routes/students.js
+++ routes/students.js
@@ -1,106 +1,17 @@
 const express = require("express")
+const { getAllStudents, getStudentById, deleteStudentById, addStudent } = require("../controller/students")
 const router = express.Router()
 const db = require("../db")
 
-router.get("/", async (_, res) => {
-    try {
-        const queryRes = await db.query("SELECT * FROM students")
-        res.send({ ...queryRes.rows })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
-})
-
-router.get("/:id", async (req, res) => {
-    try {
-        const queryRes = await db.query("SELECT * FROM students WHERE usn = $1", [req.params.id])
-        if (queryRes.rows.length > 0 )
-            res.send({ ...queryRes.rows })
-        else
-            res.send({
-                "message": "Cannot find student with id " + req.params.id
-            })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
-})
-
-router.post("/:id", async (req, res) => {
-    const query = {
-        text: `INSERT INTO students (
-                    usn,
-                    name,
-                    fathers_name,
-                    mothers_name,
-                    dob,
-                    address,
-                    phone_num,
-                    email,
-                    department,
-                    last_qualification,
-                    qualified_from,
-                    passing_year,
-                    marks_scored,
-                    percentage,
-                    cet_rank,
-                    neet_rank,
-                    aadhar_number,
-                    bank_account_number )
-                VALUES (
-                  $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
-                )
-                 RETURNING *`,
-        values: [
-            req.body.usn,
-            req.body.name,
-            req.body.fathers_name,
-            req.body.mothers_name,
-            req.body.dob,
-            req.body.address,
-            req.body.phone_num,
-            req.body.email,
-            req.body.department,
-            req.body.last_qualification,
-            req.body.qualified_from,
-            req.body.passing_year,
-            req.body.marks_scored,
-            req.body.percentage,
-            req.body.cet_rank,
-            req.body.neet_rank,
-            req.body.aadhar_number,
-            req.body.bank_account_number
-        ],
-    }
-
-    try {
-        const queryRes = await db.query(query)
-        res.send({ queryRes })
-    } catch (e) {
-        console.error(e.stack)
-        res.status(400)
-        res.send(e)
-    }
-})
+router.get("/", getAllStudents)
+router.get("/:id", getStudentById)
+router.post("/:id", addStudent)
+router.delete("/:id", deleteStudentById)
 
 router.put("/:id", (req, res) => {
     res.send({
         "msg": req.params.id + " put"
     })
-})
-
-router.delete("/:id", async (req, res) => {
-    try {
-        const queryRes = await db.query("DELETE FROM students WHERE usn = $1", [req.params.id])
-        res.send({ ...queryRes.rows })
-    } catch (e) {
-        console.error(e.stack)
-        res.statusCode(500)
-        res.send({ "message": "something went wrong" })
-    }
 })
 
 module.exports = router
