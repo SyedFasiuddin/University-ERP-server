7982ca402ff856bea6fa36dda772e9748892e284
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2022-10-22 18:53:52 +0530

    fix: handling exception

--- controller/departments.js
+++ controller/departments.js
@@ -1,16 +1,26 @@
 const db = require("../db")
 
 const getAllDepartments = async (_req, res) => {
-    const queryRes = await db.query("SELECT * FROM departments")
-    res.status(200).json({ ...queryRes.rows })
+    try {
+        const queryRes = await db.query("SELECT * FROM departments")
+        res.status(200).json({ ...queryRes.rows })
+    } catch (e) {
+        console.log(e)
+        res.status(500).end()
+    }
 }
 
 const addDepartment = async (req, res) => {
-    const queryRes = await db.query(`INSERT INTO departments
+    try {
+        const queryRes = await db.query(`INSERT INTO departments
         (full_name, short_code, hod_lecturer_id)
         VALUES ($1, $2, $3) RETURNING *`,
-        [req.body.full_name, req.body.short_code, req.body.hod_lecturer_id])
-    res.status(200).send({ ...queryRes.rows })
+            [req.body.full_name, req.body.short_code, req.body.hod_lecturer_id])
+        res.status(200).send({ ...queryRes.rows })
+    } catch (e) {
+        console.log(e)
+        res.status(500).end()
+    }
 }
 
 module.exports = {
